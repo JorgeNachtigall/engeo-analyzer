26fdb45aeec26118d757a044d7481a42fd3ec148
JorgeNachtigall <jlnvjunior@gmail.com>
2018-11-01 19:53:40 -0300

    mysql added

--- web.js
+++ web.js
@@ -1,6 +1,14 @@
 var express = require("express");
+var mysql = require('mysql');
 var app = express();
 app.use(express.logger());
+
+var connection = mysql.createConnection({
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'bf3c82b107eebd',
+    password: '7c8be2db',
+    database: 'heroku_52e91f225d931d5'
+});
 
 app.get('/', function (request, response) {
     response.send('Hello World!');
